d46ae802c8905583909c3aa49c2a56bf41f31417
steve <jedi1014@comcast.net>
2020-10-13 09:52:44 -0400

    Initial Commit

---
+++ Decision Making October 2020/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Decision_Making_October_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Decision Maker");
+            Console.Write("Please enter a number between 1 - 100: ");
+
+            string input = Console.ReadLine();
+
+            int number = int.Parse(input);
+
+            if(number % 2 > 0)
+            {
+                if(number >= 60)
+                {
+                    Console.WriteLine($"{number} is odd");
+                }
+                else
+                {
+                    Console.WriteLine("You have entered an Odd number");
+                }
+            }
+            else
+            {
+                if(number < 25)
+                {
+                    Console.WriteLine("The number entered is even and less than 25");
+                }
+                else if (number <= 60)
+                {
+                    Console.WriteLine($"{number} is even");
+                }
+                else if(number <= 100)
+                {
+
+                }
+            }
+        }
+    }
+}
